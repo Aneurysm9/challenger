5e31858e8ab21048f29d51f5ce8b5ed309dca056
Anthony J. Mirabella <a9@aneurysm9.com>
2018-03-29 01:02:03 -0400

    Finish VM implementation

--- vm/vm.go
+++ vm/vm.go
@@ -3,6 +3,7 @@ package vm
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 
 	stck "github.com/golang-collections/collections/stack"
 	log "github.com/sirupsen/logrus"
@@ -49,7 +50,7 @@ func LoadImage(fn string) (*Machine, error) {
 		m.memory[i] = uint16(img[2*i]) | (uint16(img[2*i+1]) << 8)
 	}
 
-	m.Debug = true
+	//m.Debug = true
 	log.SetLevel(log.DebugLevel)
 	return m, nil
 }
@@ -73,14 +74,40 @@ func (m *Machine) next() error {
 		return m.push()
 	case 3:
 		return m.pop()
+	case 4:
+		return m.eq()
+	case 5:
+		return m.gt()
 	case 6:
 		return m.jump()
 	case 7:
 		return m.jt()
 	case 8:
 		return m.jf()
+	case 9:
+		return m.add()
+	case 10:
+		return m.mult()
+	case 11:
+		return m.mod()
+	case 12:
+		return m.and()
+	case 13:
+		return m.or()
+	case 14:
+		return m.not()
+	case 15:
+		return m.rmem()
+	case 16:
+		return m.wmem()
+	case 17:
+		return m.call()
+	case 18:
+		return m.ret()
 	case 19:
 		return m.out()
+	case 20:
+		return m.in()
 	case 21:
 		return m.noop()
 	}
@@ -120,7 +147,7 @@ func (m *Machine) set() error {
 }
 
 func (m *Machine) push() error {
-	val := m.memory[m.ip+1]
+	val := m.getVal(m.ip + 1)
 	if m.Debug {
 		log.WithFields(log.Fields{
 			"ip":  m.ip,
@@ -146,6 +173,52 @@ func (m *Machine) pop() error {
 
 	m.setVal(dest, val.(uint16))
 	m.ip += 2
+	return nil
+}
+
+func (m *Machine) eq() error {
+	dest := m.memory[m.ip+1]
+	a := m.getVal(m.ip + 2)
+	b := m.getVal(m.ip + 3)
+
+	if m.Debug {
+		log.WithFields(log.Fields{
+			"ip":   m.ip,
+			"dest": dest,
+			"a":    a,
+			"b":    b,
+		}).Debug("Checking equality")
+	}
+
+	if a == b {
+		m.setVal(dest, 1)
+	} else {
+		m.setVal(dest, 0)
+	}
+	m.ip += 4
+	return nil
+}
+
+func (m *Machine) gt() error {
+	dest := m.memory[m.ip+1]
+	a := m.getVal(m.ip + 2)
+	b := m.getVal(m.ip + 3)
+
+	if m.Debug {
+		log.WithFields(log.Fields{
+			"ip":   m.ip,
+			"dest": dest,
+			"a":    a,
+			"b":    b,
+		}).Debug("Checking greater than")
+	}
+
+	if a > b {
+		m.setVal(dest, 1)
+	} else {
+		m.setVal(dest, 0)
+	}
+	m.ip += 4
 	return nil
 }
 
@@ -200,8 +273,193 @@ func (m *Machine) jf() error {
 	return nil
 }
 
+func (m *Machine) add() error {
+	dest := m.memory[m.ip+1]
+	a := m.getVal(m.ip + 2)
+	b := m.getVal(m.ip + 3)
+
+	if m.Debug {
+		log.WithFields(log.Fields{
+			"ip":   m.ip,
+			"dest": dest,
+			"a":    a,
+			"b":    b,
+		}).Debug("Adding numbers")
+	}
+
+	m.setVal(dest, (a+b)%maxSize)
+	m.ip += 4
+	return nil
+}
+
+func (m *Machine) mult() error {
+	dest := m.memory[m.ip+1]
+	a := m.getVal(m.ip + 2)
+	b := m.getVal(m.ip + 3)
+
+	if m.Debug {
+		log.WithFields(log.Fields{
+			"ip":   m.ip,
+			"dest": dest,
+			"a":    a,
+			"b":    b,
+		}).Debug("Multiplying numbers")
+	}
+
+	m.setVal(dest, (a*b)%maxSize)
+	m.ip += 4
+	return nil
+}
+
+func (m *Machine) mod() error {
+	dest := m.memory[m.ip+1]
+	a := m.getVal(m.ip + 2)
+	b := m.getVal(m.ip + 3)
+
+	if m.Debug {
+		log.WithFields(log.Fields{
+			"ip":   m.ip,
+			"dest": dest,
+			"a":    a,
+			"b":    b,
+		}).Debug("Modulating numbers")
+	}
+
+	m.setVal(dest, a%b)
+	m.ip += 4
+	return nil
+}
+
+func (m *Machine) and() error {
+	dest := m.memory[m.ip+1]
+	a := m.getVal(m.ip + 2)
+	b := m.getVal(m.ip + 3)
+
+	if m.Debug {
+		log.WithFields(log.Fields{
+			"ip":   m.ip,
+			"dest": dest,
+			"a":    a,
+			"b":    b,
+		}).Debug("Bitwise and")
+	}
+
+	m.setVal(dest, (a & b))
+	m.ip += 4
+	return nil
+}
+
+func (m *Machine) or() error {
+	dest := m.memory[m.ip+1]
+	a := m.getVal(m.ip + 2)
+	b := m.getVal(m.ip + 3)
+
+	if m.Debug {
+		log.WithFields(log.Fields{
+			"ip":   m.ip,
+			"dest": dest,
+			"a":    a,
+			"b":    b,
+		}).Debug("Bitwise or")
+	}
+
+	m.setVal(dest, (a | b))
+	m.ip += 4
+	return nil
+}
+
+func (m *Machine) not() error {
+	dest := m.memory[m.ip+1]
+	a := m.getVal(m.ip + 2)
+
+	if m.Debug {
+		log.WithFields(log.Fields{
+			"ip":   m.ip,
+			"dest": dest,
+			"a":    a,
+		}).Debug("Bitwise not")
+	}
+
+	a = a << 1
+	a = ^a
+	a = a >> 1
+	m.setVal(dest, a)
+	m.ip += 3
+	return nil
+}
+
+func (m *Machine) rmem() error {
+	dest := m.memory[m.ip+1]
+	a := m.memory[m.getVal(m.ip+2)]
+
+	if m.Debug {
+		log.WithFields(log.Fields{
+			"ip":   m.ip,
+			"dest": dest,
+			"a":    a,
+		}).Debug("Reading memory")
+	}
+
+	m.setVal(dest, a)
+	m.ip += 3
+	return nil
+}
+
+func (m *Machine) wmem() error {
+	dest := m.getVal(m.ip + 1)
+	a := m.getVal(m.ip + 2)
+
+	if m.Debug {
+		log.WithFields(log.Fields{
+			"ip":   m.ip,
+			"dest": dest,
+			"a":    a,
+		}).Debug("Writing memory")
+	}
+
+	m.memory[dest] = a
+	m.ip += 3
+	return nil
+}
+
+func (m *Machine) call() error {
+	dest := m.getVal(m.ip + 1)
+	if m.Debug {
+		log.WithFields(log.Fields{
+			"ip":   m.ip,
+			"dest": dest,
+		}).Debug("Calling routine")
+	}
+
+	m.stack.Push(m.ip + 2)
+	m.ip = dest
+	return nil
+}
+
+func (m *Machine) ret() error {
+	if m.stack.Len() == 0 {
+		if m.Debug {
+			log.WithFields(log.Fields{
+				"ip": m.ip,
+			}).Error("Returning from routine on empty stack")
+		}
+		return fmt.Errorf("Returning from routine on empty stack at %d", m.ip)
+	}
+
+	dest := m.stack.Pop().(uint16)
+	if m.Debug {
+		log.WithFields(log.Fields{
+			"ip":   m.ip,
+			"dest": dest,
+		}).Debug("Returning from routine")
+	}
+
+	m.ip = dest
+	return nil
+}
+
 func (m *Machine) out() error {
-	val := m.memory[m.ip+1]
+	val := m.getVal(m.ip + 1)
 	if m.Debug {
 		log.WithFields(log.Fields{
 			"ip":  m.ip,
@@ -210,6 +468,25 @@ func (m *Machine) out() error {
 		}).Debug("Printing character")
 	}
 	fmt.Printf("%c", val)
+	m.ip += 2
+	return nil
+}
+
+func (m *Machine) in() error {
+	dest := m.memory[m.ip+1]
+	b := make([]byte, 1)
+	os.Stdin.Read(b)
+	if uint16(b[0]) != 13 {
+		// Skip CR because windows is stupid
+		m.setVal(dest, uint16(b[0]))
+		if m.Debug {
+			log.WithFields(log.Fields{
+				"ip":   m.ip,
+				"dest": dest,
+				"read": b[0],
+			}).Debug("Reading character")
+		}
+	}
 	m.ip += 2
 	return nil
 }
