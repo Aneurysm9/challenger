c5651351aae4899b5f7468a5772a0c35a57539c4
Anthony J. Mirabella <a9@aneurysm9.com>
2018-03-29 00:00:21 -0400

    Jumping and register setting

--- vm/vm.go
+++ vm/vm.go
@@ -16,7 +16,7 @@ var (
 )
 
 const (
-	maxSize int = 1 << 15
+	maxSize uint16 = 1 << 15
 )
 
 // Machine represents an instance of the Synacor Challenge VM
@@ -42,13 +42,15 @@ func LoadImage(fn string) (*Machine, error) {
 
 	m := NewMachine()
 
-	for i := 0; i < maxSize; i++ {
+	for i := 0; i < int(maxSize); i++ {
 		if 2*i >= len(img) {
 			break
 		}
 		m.memory[i] = uint16(img[2*i]) | (uint16(img[2*i+1]) << 8)
 	}
 
+	m.Debug = true
+	log.SetLevel(log.DebugLevel)
 	return m, nil
 }
 
@@ -65,13 +67,137 @@ func (m *Machine) next() error {
 	switch m.memory[m.ip] {
 	case 0:
 		return ErrorHalt
+	case 1:
+		return m.set()
+	case 2:
+		return m.push()
+	case 3:
+		return m.pop()
+	case 6:
+		return m.jump()
+	case 7:
+		return m.jt()
+	case 8:
+		return m.jf()
 	case 19:
 		return m.out()
 	case 21:
 		return m.noop()
 	}
 
+	log.WithFields(log.Fields{
+		"ip":    m.ip,
+		"instr": m.memory[m.ip],
+	}).Fatal("Unknown instruction")
 	return ErrorUnknownInstruction
+}
+
+func (m *Machine) set() error {
+	idx := m.memory[m.ip+1] % maxSize
+	if idx > 7 {
+		log.WithFields(log.Fields{
+			"ip":  m.ip,
+			"op":  "set",
+			"idx": idx,
+		}).Error("Attempt to set an invalid register")
+		return fmt.Errorf("Attempt to set an invalid register")
+	}
+
+	val := m.getVal(m.ip + 2)
+
+	if m.Debug {
+		log.WithFields(log.Fields{
+			"ip":  m.ip,
+			"op":  "set",
+			"idx": idx,
+			"val": val,
+		}).Debug("Setting register")
+	}
+	m.registers[idx] = val
+
+	m.ip += 3
+	return nil
+}
+
+func (m *Machine) push() error {
+	val := m.memory[m.ip+1]
+	if m.Debug {
+		log.WithFields(log.Fields{
+			"ip":  m.ip,
+			"val": val,
+		}).Debug("Pushing value onto stack")
+	}
+
+	m.stack.Push(val)
+	m.ip += 2
+	return nil
+}
+
+func (m *Machine) pop() error {
+	dest := m.memory[m.ip+1]
+	val := m.stack.Pop()
+	if m.Debug {
+		log.WithFields(log.Fields{
+			"ip":   m.ip,
+			"dest": dest,
+			"val":  val,
+		}).Debug("Popping value from stack")
+	}
+
+	m.setVal(dest, val.(uint16))
+	m.ip += 2
+	return nil
+}
+
+func (m *Machine) jump() error {
+	dest := m.memory[m.ip+1]
+	if m.Debug {
+		log.WithFields(log.Fields{
+			"ip":   m.ip,
+			"dest": dest,
+		}).Debug("Jumping")
+	}
+
+	m.ip = dest
+	return nil
+}
+
+func (m *Machine) jt() error {
+	val := m.getVal(m.ip + 1)
+	dest := m.getVal(m.ip + 2)
+	if m.Debug {
+		log.WithFields(log.Fields{
+			"ip":   m.ip,
+			"dest": dest,
+			"val":  val,
+		}).Debug("Jumping if true")
+	}
+
+	if val != 0 {
+		m.ip = dest
+	} else {
+		m.ip += 3
+	}
+	return nil
+}
+
+func (m *Machine) jf() error {
+	val := m.getVal(m.ip + 1)
+	dest := m.getVal(m.ip + 2)
+	if m.Debug {
+		log.WithFields(log.Fields{
+			"ip":   m.ip,
+			"dest": dest,
+			"val":  val,
+		}).Debug("Jumping if false")
+	}
+
+	if val == 0 {
+		m.ip = dest
+	} else {
+		m.ip += 3
+	}
+	return nil
 }
 
 func (m *Machine) out() error {
@@ -97,4 +223,28 @@ func (m *Machine) noop() error {
 	}
 	m.ip++
 	return nil
+}
+
+func (m *Machine) getVal(loc uint16) uint16 {
+	val := m.memory[loc]
+	if isReg(val) {
+		return m.registers[val%maxSize]
+	}
+	return val
+}
+
+func (m *Machine) setVal(loc, val uint16) {
+	if isReg(loc) {
+		m.registers[loc%maxSize] = val
+	} else {
+		m.memory[loc] = val
+	}
+}
+
+func isMem(v uint16) bool {
+	return v < maxSize
+}
+
+func isReg(v uint16) bool {
+	return v >= maxSize
 }
